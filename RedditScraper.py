8801cf5a694a0f70399ace4533907771dc4cfb7f
Jason Abohwo <jabohwho@gmail.com>
2022-09-21 15:46:45 -0500

    Update RedditScraper.py

--- RedditScraper.py
+++ RedditScraper.py
@@ -5,11 +5,16 @@ import praw
 from praw.models import MoreComments
 from collections import OrderedDict
 import pandas as pd
+from psaw import PushshiftAPI
+import datetime as dt
+
 
 class Scraper:
     def __init__(self, id, secret, agent, subreddit) -> None:
         self.reddit = praw.Reddit(client_id=id, client_secret=secret, user_agent=agent)
+        self.api = PushshiftAPI()
         self.subreddit = self.reddit.subreddit(subreddit)
+
 
     def check_flairs(self, post, barred_flairs):
         for flair in barred_flairs:
@@ -26,7 +31,8 @@ class Scraper:
     def get_stories(self, limit, comments=False, barred_flairs=[]):
         stories = []
         num_stories = 0
-        for post in list(self.subreddit.hot())[1:]:
+        start = int(dt.datetime(2014, 1, 1).timestamp())
+        for post in list(self.api.search_submissions(after=start, subreddit='nosleep', limit=limit))[1:]:
             post_data = self.extract_data(post, comments)
 
             if post_data[1] is None:
@@ -82,9 +88,16 @@ def get_nosleep_data(id, secret, agent, limit):
     return scraper.get_stories(limit, comments=False, barred_flairs=['series'])
 
 def get_AITA_data(id, secret, agent, limit):
-    scraper = Scraper(id, secret, agent, 'nosleep')
-    return scraper.get_stories(limit, comments=False, barred_flairs=['series'])
+    scraper = Scraper(id, secret, agent, 'AmITheAsshole')
+    return scraper.get_stories(limit, comments=False, barred_flairs=[])
     
 if __name__ == '__main__':
-    print(get_nosleep_data(id='x4xepFGg0oNAZYWp1z9qXg', secret='VJQziUlLzwHviqHU1EWhCMaCJaXBSg', agent='AITACommentary', limit=1)[0])
+    nslp_data = get_AITA_data(id='x4xepFGg0oNAZYWp1z9qXg', secret='VJQziUlLzwHviqHU1EWhCMaCJaXBSg', agent='AITACommentary', limit=1000000)
+    categories = []
+    titles = []
+    for datapoint in nslp_data:
+        categories.append([1,0])
+        titles.append(datapoint[0])
+    data = pd.DataFrame({"category":categories, "title":titles})
+    data.to_pickle("nosleep_data.pkl")
 

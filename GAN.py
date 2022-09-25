b6ea5cc254cd31cd22f2936b2625fd7d21f2bbec
Jason Abohwo <jabohwho@gmail.com>
2022-09-25 17:16:53 -0500

    Update GAN.py

--- GAN.py
+++ GAN.py
@@ -1,4 +1,5 @@
 from base64 import encode
+from cgitb import text
 from hashlib import new
 from msilib import sequence
 import tensorflow as tf
@@ -19,6 +20,7 @@ from multiprocessing import pool
 from Encoder import Encoder
 from Generator import Generator
 from Discriminator import Discriminator
+from AutoEncoder import AutoEncoder
 
 
 
@@ -29,12 +31,14 @@ class GAN(Model):
         self.encoder = Encoder(text, maxlen)
         self.generator = Generator(self.encoder.vocab_len, self.encoder.sequence_maxlen)
         self.discriminator = Discriminator()
+        self.ae = AutoEncoder(self.encoder.vocab_len, self.encoder.sequence_maxlen)
 
         self.gen_opt = Adam(1e-5)
         self.discrim_opt = Adam(1e-7)
 
         self.generator.compile(optimizer=self.gen_opt)
         self.discriminator.compile(optimizer=self.discrim_opt)
+        self.ae.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
         self.vocab_len = self.encoder.vocab_len
         self.sequence_len = self.encoder.sequence_maxlen
@@ -84,6 +88,22 @@ class GAN(Model):
         )
         return checkpoint, checkpoint_prefix
 
+    def train_autoencoder(self, text_data, epochs=100, verbose=1):
+        formatted_data = []
+        for datapoint in text_data:
+            x = self.encoder.encode(datapoint)
+            formatted_data.append(x)
+        formatted_data = np.reshape(formatted_data, (len(text_data),self.sequence_len,self.vocab_len))
+        self.ae.fit(formatted_data, formatted_data, epochs=epochs, verbose=verbose, batch_size=32)
+    
+    def test_autoencoder(self, text_data, verbose=1):
+        formatted_data = []
+        for datapoint in text_data:
+            x = self.encoder.encode(datapoint)
+            formatted_data.append(x)
+        formatted_data = np.reshape(formatted_data, (len(text_data),self.sequence_len,self.vocab_len))
+        self.ae.evaluate(formatted_data, formatted_data, verbose)
+
     def create_text_representation(self, generator_text, timesteps):
         new_generator_text = []
         marker = 0
@@ -98,39 +118,37 @@ class GAN(Model):
     def trainstep(self, real_texts, noises):
         generator_gradients = []
         discriminator_gradients = []
+        generated_texts = []
         for real_text, noise in zip(real_texts, noises):
             with tf.GradientTape() as tape1, tf.GradientTape() as tape2:
-                generator_text = [[0]*self.vocab_len for _ in range(self.sequence_len)]
-                for timestep in range(self.sequence_len):
-                    fake_text = self.generator(noise, tf.cast(tf.reshape(generator_text, (1, self.sequence_len, self.vocab_len)), tf.float32), training=True)
-                    generator_text[timestep] = fake_text[0]
+                
+                fake_text = self.generator(noise, training=True)
+                fake_text = tf.cast(fake_text, dtype=tf.float32)
+                fake_text = self.ae.decode(fake_text)
+                generated_texts.append(fake_text)
 
-                    #fake_text = tf.cast(fake_text, dtype=tf.float32)
-                    #fake_text = tf.reshape(fake_text, (1,self.encoder.sequence_maxlen, self.encoder.vocab_len))
-                    #fake_text += 0.05 * tf.random.uniform(tf.shape(fake_text))
+                
+                fake_text = tf.reshape(fake_text, (1,self.encoder.sequence_maxlen, self.encoder.vocab_len))
+                #fake_text += 0.05 * tf.random.uniform(tf.shape(fake_text))
 
+                real_text = real_text[0]
                 real_text = tf.reshape(real_text, (1,self.encoder.sequence_maxlen, self.encoder.vocab_len))
-                real_text = 0.05 * tf.random.uniform(tf.shape(real_text))
+                #real_text = 0.05 * tf.random.uniform(tf.shape(real_text))
 
-
-                fake_text_y = self.discriminator(tf.reshape(generator_text, (1,self.sequence_len, self.vocab_len)), training=True)
+                fake_text_y = self.discriminator(fake_text, training=True)
                 real_text_y = self.discriminator(real_text, training=True)
 
-                generator_loss, discriminator_loss = self.wasserstein_loss(real_text_y, fake_text_y)
-            
-                    
-                generator_gradients.append(tape1.gradient(generator_loss, self.generator.trainable_variables))
-                discriminator_gradients.append(tape2.gradient(discriminator_loss, self.discriminator.trainable_variables))
-       
+                generator_loss, discriminator_loss = self.minmax_loss(real_text_y, fake_text_y)
 
+            generator_gradients.append(tape1.gradient(generator_loss, self.generator.trainable_variables))
+            discriminator_gradients.append(tape2.gradient(discriminator_loss, self.discriminator.trainable_variables))
         
         for g_grad, d_grad in zip(generator_gradients, discriminator_gradients):
-            
             self.gen_opt.apply_gradients(zip(g_grad, self.generator.trainable_variables))
             self.discrim_opt.apply_gradients(zip(d_grad, self.discriminator.trainable_variables))
-                    
-        return generator_loss, discriminator_loss, generator_text, fake_text_y, real_text_y
-
+        
+        return generator_loss, discriminator_loss, generated_texts
+       
     def format_tokens(self, tokens):
         new_tokens = np.zeros((self.sequence_len, self.sequence_len, self.vocab_len))
         for timestep in range(new_tokens.shape[0]):
@@ -167,6 +185,7 @@ class GAN(Model):
                     if len([token[0] for token in tokens[0]]) > self.encoder.sequence_maxlen:
                         continue
                     
+                    tokens = self.ae.predict(np.expand_dims(tokens, axis=0), verbose=0)
 
 
 
@@ -180,14 +199,13 @@ class GAN(Model):
 
 
 
-                g_loss, d_loss, generated_text, fdi, rdi = self.trainstep(token_batch, noise_batch)
+                g_loss, d_loss, generated_text = self.trainstep(token_batch, noise_batch)
                 generated_text = self.encoder.decode(generated_text)
 
                 if int(marker/batch_size) % print_freq == 0:
                     print(f"""Epoch: {epoch} | Batch: {int(marker/batch_size)} | Time: {time.time()-start_time}\n
                         Generator loss: {g_loss} | Discriminator loss: {d_loss}\n
-                        Example text 1: {generated_text}\n
-                        {fdi} {rdi}"""
+                        Example text 1: {generated_text}\n"""
                     )
 
                 if int(marker/batch_size) % ckpt_freq == 0:
